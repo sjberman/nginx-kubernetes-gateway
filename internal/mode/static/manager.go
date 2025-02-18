4e1a807e1ab2046eed42fe4c9735ae0b22722f18
salonichf5 <146118978+salonichf5@users.noreply.github.com>
2025-02-18 11:10:13 -0700

    Fix revert commit for leader election (#3136)
    * Add back runnables change and call to nginx provisioner enable
    
    ---------
    
    Co-authored-by: Benjamin Jee <b.jee@f5.com>

--- internal/mode/static/manager.go
+++ internal/mode/static/manager.go
@@ -262,6 +262,13 @@ func StartManager(cfg config.Config) error {
 		return fmt.Errorf("cannot register event loop: %w", err)
 	}
 
+	if err = mgr.Add(runnables.NewCallFunctionsAfterBecameLeader([]func(context.Context){
+		groupStatusUpdater.Enable,
+		nginxProvisioner.Enable,
+	})); err != nil {
+		return fmt.Errorf("cannot register functions that get called after Pod becomes leader: %w", err)
+	}
+
 	if cfg.ProductTelemetryConfig.Enabled {
 		dataCollector := telemetry.NewDataCollectorImpl(telemetry.DataCollectorConfig{
 			K8sClientReader:     mgr.GetAPIReader(),
