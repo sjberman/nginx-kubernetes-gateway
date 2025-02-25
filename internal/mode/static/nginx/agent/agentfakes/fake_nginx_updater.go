nginxinc/nginx-gateway-fabric

CP/DP Split: write configuration to agent (#2999)
This commit adds functionality to send nginx configuration to the agent. It also adds support for the single nginx Deployment to be scaled, and send configuration to all replicas. This requires tracking all Subscriptions for a particular deployment, and receiving all responses from those replicas to determine the status to write to the Gateway.

internal/mode/static/nginx/agent/agentfakes/fake_nginx_updater.go
@@ -5,33 +5,61 @@ import (
 	"sync"
 
 	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/nginx/agent"
+	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/state/dataplane"
 )
 
 type FakeNginxUpdater struct {
-	UpdateConfigStub        func(int)
+	UpdateConfigStub        func(*agent.Deployment, []agent.File) bool
 	updateConfigMutex       sync.RWMutex
 	updateConfigArgsForCall []struct {
-		arg1 int
+		arg1 *agent.Deployment
+		arg2 []agent.File
 	}
-	UpdateUpstreamServersStub        func()
+	updateConfigReturns struct {
+		result1 bool
+	}
+	updateConfigReturnsOnCall map[int]struct {
+		result1 bool
+	}
+	UpdateUpstreamServersStub        func(*agent.Deployment, dataplane.Configuration) bool
 	updateUpstreamServersMutex       sync.RWMutex
 	updateUpstreamServersArgsForCall []struct {
+		arg1 *agent.Deployment
+		arg2 dataplane.Configuration
+	}
+	updateUpstreamServersReturns struct {
+		result1 bool
+	}
+	updateUpstreamServersReturnsOnCall map[int]struct {
+		result1 bool
 	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeNginxUpdater) UpdateConfig(arg1 int) {
+func (fake *FakeNginxUpdater) UpdateConfig(arg1 *agent.Deployment, arg2 []agent.File) bool {
+	var arg2Copy []agent.File
+	if arg2 != nil {
+		arg2Copy = make([]agent.File, len(arg2))
+		copy(arg2Copy, arg2)
+	}
 	fake.updateConfigMutex.Lock()
+	ret, specificReturn := fake.updateConfigReturnsOnCall[len(fake.updateConfigArgsForCall)]
 	fake.updateConfigArgsForCall = append(fake.updateConfigArgsForCall, struct {
-		arg1 int
-	}{arg1})
+		arg1 *agent.Deployment
+		arg2 []agent.File
+	}{arg1, arg2Copy})
 	stub := fake.UpdateConfigStub
-	fake.recordInvocation("UpdateConfig", []interface{}{arg1})
+	fakeReturns := fake.updateConfigReturns
+	fake.recordInvocation("UpdateConfig", []interface{}{arg1, arg2Copy})
 	fake.updateConfigMutex.Unlock()
 	if stub != nil {
-		fake.UpdateConfigStub(arg1)
+		return stub(arg1, arg2)
 	}
+	if specificReturn {
+		return ret.result1
+	}
+	return fakeReturns.result1
 }
 
 func (fake *FakeNginxUpdater) UpdateConfigCallCount() int {
@@ -40,29 +68,60 @@ func (fake *FakeNginxUpdater) UpdateConfigCallCount() int {
 	return len(fake.updateConfigArgsForCall)
 }
 
-func (fake *FakeNginxUpdater) UpdateConfigCalls(stub func(int)) {
+func (fake *FakeNginxUpdater) UpdateConfigCalls(stub func(*agent.Deployment, []agent.File) bool) {
 	fake.updateConfigMutex.Lock()
 	defer fake.updateConfigMutex.Unlock()
 	fake.UpdateConfigStub = stub
 }
 
-func (fake *FakeNginxUpdater) UpdateConfigArgsForCall(i int) int {
+func (fake *FakeNginxUpdater) UpdateConfigArgsForCall(i int) (*agent.Deployment, []agent.File) {
 	fake.updateConfigMutex.RLock()
 	defer fake.updateConfigMutex.RUnlock()
 	argsForCall := fake.updateConfigArgsForCall[i]
-	return argsForCall.arg1
+	return argsForCall.arg1, argsForCall.arg2
 }
 
-func (fake *FakeNginxUpdater) UpdateUpstreamServers() {
+func (fake *FakeNginxUpdater) UpdateConfigReturns(result1 bool) {
+	fake.updateConfigMutex.Lock()
+	defer fake.updateConfigMutex.Unlock()
+	fake.UpdateConfigStub = nil
+	fake.updateConfigReturns = struct {
+		result1 bool
+	}{result1}
+}
+
+func (fake *FakeNginxUpdater) UpdateConfigReturnsOnCall(i int, result1 bool) {
+	fake.updateConfigMutex.Lock()
+	defer fake.updateConfigMutex.Unlock()
+	fake.UpdateConfigStub = nil
+	if fake.updateConfigReturnsOnCall == nil {
+		fake.updateConfigReturnsOnCall = make(map[int]struct {
+			result1 bool
+		})
+	}
+	fake.updateConfigReturnsOnCall[i] = struct {
+		result1 bool
+	}{result1}
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServers(arg1 *agent.Deployment, arg2 dataplane.Configuration) bool {
 	fake.updateUpstreamServersMutex.Lock()
+	ret, specificReturn := fake.updateUpstreamServersReturnsOnCall[len(fake.updateUpstreamServersArgsForCall)]
 	fake.updateUpstreamServersArgsForCall = append(fake.updateUpstreamServersArgsForCall, struct {
-	}{})
+		arg1 *agent.Deployment
+		arg2 dataplane.Configuration
+	}{arg1, arg2})
 	stub := fake.UpdateUpstreamServersStub
-	fake.recordInvocation("UpdateUpstreamServers", []interface{}{})
+	fakeReturns := fake.updateUpstreamServersReturns
+	fake.recordInvocation("UpdateUpstreamServers", []interface{}{arg1, arg2})
 	fake.updateUpstreamServersMutex.Unlock()
 	if stub != nil {
-		fake.UpdateUpstreamServersStub()
+		return stub(arg1, arg2)
 	}
+	if specificReturn {
+		return ret.result1
+	}
+	return fakeReturns.result1
 }
 
 func (fake *FakeNginxUpdater) UpdateUpstreamServersCallCount() int {
@@ -71,10 +130,40 @@ func (fake *FakeNginxUpdater) UpdateUpstreamServersCallCount() int {
 	return len(fake.updateUpstreamServersArgsForCall)
 }
 
-func (fake *FakeNginxUpdater) UpdateUpstreamServersCalls(stub func()) {
+func (fake *FakeNginxUpdater) UpdateUpstreamServersCalls(stub func(*agent.Deployment, dataplane.Configuration) bool) {
 	fake.updateUpstreamServersMutex.Lock()
 	defer fake.updateUpstreamServersMutex.Unlock()
 	fake.UpdateUpstreamServersStub = stub
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServersArgsForCall(i int) (*agent.Deployment, dataplane.Configuration) {
+	fake.updateUpstreamServersMutex.RLock()
+	defer fake.updateUpstreamServersMutex.RUnlock()
+	argsForCall := fake.updateUpstreamServersArgsForCall[i]
+	return argsForCall.arg1, argsForCall.arg2
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServersReturns(result1 bool) {
+	fake.updateUpstreamServersMutex.Lock()
+	defer fake.updateUpstreamServersMutex.Unlock()
+	fake.UpdateUpstreamServersStub = nil
+	fake.updateUpstreamServersReturns = struct {
+		result1 bool
+	}{result1}
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServersReturnsOnCall(i int, result1 bool) {
+	fake.updateUpstreamServersMutex.Lock()
+	defer fake.updateUpstreamServersMutex.Unlock()
+	fake.UpdateUpstreamServersStub = nil
+	if fake.updateUpstreamServersReturnsOnCall == nil {
+		fake.updateUpstreamServersReturnsOnCall = make(map[int]struct {
+			result1 bool
+		})
+	}
+	fake.updateUpstreamServersReturnsOnCall[i] = struct {
+		result1 bool
+	}{result1}
 }
 
 func (fake *FakeNginxUpdater) Invocations() map[string][][]interface{} {

internal/mode/static/nginx/agent/grpc/messenger/messenger_test.go
@@ -1,0 +1,125 @@
+package messenger_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	v1 "github.com/nginx/agent/v3/api/grpc/mpi/v1"
+	. "github.com/onsi/gomega"
+	"google.golang.org/grpc"
+
+	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/nginx/agent/grpc/messenger"
+)
+
+type mockServer struct {
+	grpc.ServerStream
+	sendChan chan *v1.ManagementPlaneRequest
+	recvChan chan *v1.DataPlaneResponse
+}
+
+func (m *mockServer) Send(msg *v1.ManagementPlaneRequest) error {
+	m.sendChan <- msg
+	return nil
+}
+
+func (m *mockServer) Recv() (*v1.DataPlaneResponse, error) {
+	msg, ok := <-m.recvChan
+	if !ok {
+		return nil, errors.New("channel closed")
+	}
+	return msg, nil
+}
+
+type mockErrorServer struct {
+	grpc.ServerStream
+	sendChan chan *v1.ManagementPlaneRequest
+	recvChan chan *v1.DataPlaneResponse
+}
+
+func (m *mockErrorServer) Send(_ *v1.ManagementPlaneRequest) error {
+	return errors.New("error sending to server")
+}
+
+func (m *mockErrorServer) Recv() (*v1.DataPlaneResponse, error) {
+	<-m.recvChan
+	return nil, errors.New("error received from server")
+}
+
+func createServer() *mockServer {
+	return &mockServer{
+		sendChan: make(chan *v1.ManagementPlaneRequest, 1),
+		recvChan: make(chan *v1.DataPlaneResponse, 1),
+	}
+}
+
+func createErrorServer() *mockErrorServer {
+	return &mockErrorServer{
+		sendChan: make(chan *v1.ManagementPlaneRequest, 1),
+		recvChan: make(chan *v1.DataPlaneResponse, 1),
+	}
+}
+
+func TestSend(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := createServer()
+	msgr := messenger.New(server)
+
+	go msgr.Run(ctx)
+
+	msg := &v1.ManagementPlaneRequest{
+		MessageMeta: &v1.MessageMeta{
+			MessageId: "test",
+		},
+	}
+	g.Expect(msgr.Send(ctx, msg)).To(Succeed())
+
+	g.Eventually(server.sendChan).Should(Receive(Equal(msg)))
+
+	cancel()
+
+	g.Expect(msgr.Send(ctx, &v1.ManagementPlaneRequest{})).ToNot(Succeed())
+}
+
+func TestMessages(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := createServer()
+	msgr := messenger.New(server)
+
+	go msgr.Run(ctx)
+
+	msg := &v1.DataPlaneResponse{InstanceId: "test"}
+	server.recvChan <- msg
+
+	g.Eventually(msgr.Messages()).Should(Receive(Equal(msg)))
+}
+
+func TestErrors(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := createErrorServer()
+	msgr := messenger.New(server)
+
+	go msgr.Run(ctx)
+
+	g.Expect(msgr.Send(ctx, &v1.ManagementPlaneRequest{})).To(Succeed())
+	g.Eventually(msgr.Errors()).Should(Receive(MatchError("error sending to server")))
+
+	server.recvChan <- &v1.DataPlaneResponse{}
+
+	g.Eventually(msgr.Errors()).Should(Receive(MatchError("error received from server")))
+}

internal/mode/static/nginx/config/configfakes/fake_generator.go
@@ -4,41 +4,41 @@ package configfakes
 import (
 	"sync"
 
-	"github.com/nginxinc/nginx-gateway-fabric/internal/framework/file"
+	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/nginx/agent"
 	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/nginx/config"
 	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/state/dataplane"
 )
 
 type FakeGenerator struct {
-	GenerateStub        func(dataplane.Configuration) []file.File
+	GenerateStub        func(dataplane.Configuration) []agent.File
 	generateMutex       sync.RWMutex
 	generateArgsForCall []struct {
 		arg1 dataplane.Configuration
 	}
 	generateReturns struct {
-		result1 []file.File
+		result1 []agent.File
 	}
 	generateReturnsOnCall map[int]struct {
-		result1 []file.File
+		result1 []agent.File
 	}
-	GenerateDeploymentContextStub        func(dataplane.DeploymentContext) (file.File, error)
+	GenerateDeploymentContextStub        func(dataplane.DeploymentContext) (agent.File, error)
 	generateDeploymentContextMutex       sync.RWMutex
 	generateDeploymentContextArgsForCall []struct {
 		arg1 dataplane.DeploymentContext
 	}
 	generateDeploymentContextReturns struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}
 	generateDeploymentContextReturnsOnCall map[int]struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeGenerator) Generate(arg1 dataplane.Configuration) []file.File {
+func (fake *FakeGenerator) Generate(arg1 dataplane.Configuration) []agent.File {
 	fake.generateMutex.Lock()
 	ret, specificReturn := fake.generateReturnsOnCall[len(fake.generateArgsForCall)]
 	fake.generateArgsForCall = append(fake.generateArgsForCall, struct {
@@ -63,7 +63,7 @@ func (fake *FakeGenerator) GenerateCallCount() int {
 	return len(fake.generateArgsForCall)
 }
 
-func (fake *FakeGenerator) GenerateCalls(stub func(dataplane.Configuration) []file.File) {
+func (fake *FakeGenerator) GenerateCalls(stub func(dataplane.Configuration) []agent.File) {
 	fake.generateMutex.Lock()
 	defer fake.generateMutex.Unlock()
 	fake.GenerateStub = stub
@@ -76,30 +76,30 @@ func (fake *FakeGenerator) GenerateArgsForCall(i int) dataplane.Configuration {
 	return argsForCall.arg1
 }
 
-func (fake *FakeGenerator) GenerateReturns(result1 []file.File) {
+func (fake *FakeGenerator) GenerateReturns(result1 []agent.File) {
 	fake.generateMutex.Lock()
 	defer fake.generateMutex.Unlock()
 	fake.GenerateStub = nil
 	fake.generateReturns = struct {
-		result1 []file.File
+		result1 []agent.File
 	}{result1}
 }
 
-func (fake *FakeGenerator) GenerateReturnsOnCall(i int, result1 []file.File) {
+func (fake *FakeGenerator) GenerateReturnsOnCall(i int, result1 []agent.File) {
 	fake.generateMutex.Lock()
 	defer fake.generateMutex.Unlock()
 	fake.GenerateStub = nil
 	if fake.generateReturnsOnCall == nil {
 		fake.generateReturnsOnCall = make(map[int]struct {
-			result1 []file.File
+			result1 []agent.File
 		})
 	}
 	fake.generateReturnsOnCall[i] = struct {
-		result1 []file.File
+		result1 []agent.File
 	}{result1}
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContext(arg1 dataplane.DeploymentContext) (file.File, error) {
+func (fake *FakeGenerator) GenerateDeploymentContext(arg1 dataplane.DeploymentContext) (agent.File, error) {
 	fake.generateDeploymentContextMutex.Lock()
 	ret, specificReturn := fake.generateDeploymentContextReturnsOnCall[len(fake.generateDeploymentContextArgsForCall)]
 	fake.generateDeploymentContextArgsForCall = append(fake.generateDeploymentContextArgsForCall, struct {
@@ -124,7 +124,7 @@ func (fake *FakeGenerator) GenerateDeploymentContextCallCount() int {
 	return len(fake.generateDeploymentContextArgsForCall)
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContextCalls(stub func(dataplane.DeploymentContext) (file.File, error)) {
+func (fake *FakeGenerator) GenerateDeploymentContextCalls(stub func(dataplane.DeploymentContext) (agent.File, error)) {
 	fake.generateDeploymentContextMutex.Lock()
 	defer fake.generateDeploymentContextMutex.Unlock()
 	fake.GenerateDeploymentContextStub = stub
@@ -137,28 +137,28 @@ func (fake *FakeGenerator) GenerateDeploymentContextArgsForCall(i int) dataplane
 	return argsForCall.arg1
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContextReturns(result1 file.File, result2 error) {
+func (fake *FakeGenerator) GenerateDeploymentContextReturns(result1 agent.File, result2 error) {
 	fake.generateDeploymentContextMutex.Lock()
 	defer fake.generateDeploymentContextMutex.Unlock()
 	fake.GenerateDeploymentContextStub = nil
 	fake.generateDeploymentContextReturns = struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}{result1, result2}
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContextReturnsOnCall(i int, result1 file.File, result2 error) {
+func (fake *FakeGenerator) GenerateDeploymentContextReturnsOnCall(i int, result1 agent.File, result2 error) {
 	fake.generateDeploymentContextMutex.Lock()
 	defer fake.generateDeploymentContextMutex.Unlock()
 	fake.GenerateDeploymentContextStub = nil
 	if fake.generateDeploymentContextReturnsOnCall == nil {
 		fake.generateDeploymentContextReturnsOnCall = make(map[int]struct {
-			result1 file.File
+			result1 agent.File
 			result2 error
 		})
 	}
 	fake.generateDeploymentContextReturnsOnCall[i] = struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}{result1, result2}
 }

sjberman/nginx-kubernetes-gateway

internal/mode/static/nginx/agent/agentfakes/fake_nginx_updater.go
@@ -5,33 +5,61 @@ import (
 	"sync"
 
 	"github.com/nginx/nginx-gateway-fabric/internal/mode/static/nginx/agent"
+	"github.com/nginx/nginx-gateway-fabric/internal/mode/static/state/dataplane"
 )
 
 type FakeNginxUpdater struct {
-	UpdateConfigStub        func(int)
+	UpdateConfigStub        func(*agent.Deployment, []agent.File) bool
 	updateConfigMutex       sync.RWMutex
 	updateConfigArgsForCall []struct {
-		arg1 int
+		arg1 *agent.Deployment
+		arg2 []agent.File
 	}
-	UpdateUpstreamServersStub        func()
+	updateConfigReturns struct {
+		result1 bool
+	}
+	updateConfigReturnsOnCall map[int]struct {
+		result1 bool
+	}
+	UpdateUpstreamServersStub        func(*agent.Deployment, dataplane.Configuration) bool
 	updateUpstreamServersMutex       sync.RWMutex
 	updateUpstreamServersArgsForCall []struct {
+		arg1 *agent.Deployment
+		arg2 dataplane.Configuration
+	}
+	updateUpstreamServersReturns struct {
+		result1 bool
+	}
+	updateUpstreamServersReturnsOnCall map[int]struct {
+		result1 bool
 	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeNginxUpdater) UpdateConfig(arg1 int) {
+func (fake *FakeNginxUpdater) UpdateConfig(arg1 *agent.Deployment, arg2 []agent.File) bool {
+	var arg2Copy []agent.File
+	if arg2 != nil {
+		arg2Copy = make([]agent.File, len(arg2))
+		copy(arg2Copy, arg2)
+	}
 	fake.updateConfigMutex.Lock()
+	ret, specificReturn := fake.updateConfigReturnsOnCall[len(fake.updateConfigArgsForCall)]
 	fake.updateConfigArgsForCall = append(fake.updateConfigArgsForCall, struct {
-		arg1 int
-	}{arg1})
+		arg1 *agent.Deployment
+		arg2 []agent.File
+	}{arg1, arg2Copy})
 	stub := fake.UpdateConfigStub
-	fake.recordInvocation("UpdateConfig", []interface{}{arg1})
+	fakeReturns := fake.updateConfigReturns
+	fake.recordInvocation("UpdateConfig", []interface{}{arg1, arg2Copy})
 	fake.updateConfigMutex.Unlock()
 	if stub != nil {
-		fake.UpdateConfigStub(arg1)
+		return stub(arg1, arg2)
 	}
+	if specificReturn {
+		return ret.result1
+	}
+	return fakeReturns.result1
 }
 
 func (fake *FakeNginxUpdater) UpdateConfigCallCount() int {
@@ -40,29 +68,60 @@ func (fake *FakeNginxUpdater) UpdateConfigCallCount() int {
 	return len(fake.updateConfigArgsForCall)
 }
 
-func (fake *FakeNginxUpdater) UpdateConfigCalls(stub func(int)) {
+func (fake *FakeNginxUpdater) UpdateConfigCalls(stub func(*agent.Deployment, []agent.File) bool) {
 	fake.updateConfigMutex.Lock()
 	defer fake.updateConfigMutex.Unlock()
 	fake.UpdateConfigStub = stub
 }
 
-func (fake *FakeNginxUpdater) UpdateConfigArgsForCall(i int) int {
+func (fake *FakeNginxUpdater) UpdateConfigArgsForCall(i int) (*agent.Deployment, []agent.File) {
 	fake.updateConfigMutex.RLock()
 	defer fake.updateConfigMutex.RUnlock()
 	argsForCall := fake.updateConfigArgsForCall[i]
-	return argsForCall.arg1
+	return argsForCall.arg1, argsForCall.arg2
 }
 
-func (fake *FakeNginxUpdater) UpdateUpstreamServers() {
+func (fake *FakeNginxUpdater) UpdateConfigReturns(result1 bool) {
+	fake.updateConfigMutex.Lock()
+	defer fake.updateConfigMutex.Unlock()
+	fake.UpdateConfigStub = nil
+	fake.updateConfigReturns = struct {
+		result1 bool
+	}{result1}
+}
+
+func (fake *FakeNginxUpdater) UpdateConfigReturnsOnCall(i int, result1 bool) {
+	fake.updateConfigMutex.Lock()
+	defer fake.updateConfigMutex.Unlock()
+	fake.UpdateConfigStub = nil
+	if fake.updateConfigReturnsOnCall == nil {
+		fake.updateConfigReturnsOnCall = make(map[int]struct {
+			result1 bool
+		})
+	}
+	fake.updateConfigReturnsOnCall[i] = struct {
+		result1 bool
+	}{result1}
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServers(arg1 *agent.Deployment, arg2 dataplane.Configuration) bool {
 	fake.updateUpstreamServersMutex.Lock()
+	ret, specificReturn := fake.updateUpstreamServersReturnsOnCall[len(fake.updateUpstreamServersArgsForCall)]
 	fake.updateUpstreamServersArgsForCall = append(fake.updateUpstreamServersArgsForCall, struct {
-	}{})
+		arg1 *agent.Deployment
+		arg2 dataplane.Configuration
+	}{arg1, arg2})
 	stub := fake.UpdateUpstreamServersStub
-	fake.recordInvocation("UpdateUpstreamServers", []interface{}{})
+	fakeReturns := fake.updateUpstreamServersReturns
+	fake.recordInvocation("UpdateUpstreamServers", []interface{}{arg1, arg2})
 	fake.updateUpstreamServersMutex.Unlock()
 	if stub != nil {
-		fake.UpdateUpstreamServersStub()
+		return stub(arg1, arg2)
 	}
+	if specificReturn {
+		return ret.result1
+	}
+	return fakeReturns.result1
 }
 
 func (fake *FakeNginxUpdater) UpdateUpstreamServersCallCount() int {
@@ -71,10 +130,40 @@ func (fake *FakeNginxUpdater) UpdateUpstreamServersCallCount() int {
 	return len(fake.updateUpstreamServersArgsForCall)
 }
 
-func (fake *FakeNginxUpdater) UpdateUpstreamServersCalls(stub func()) {
+func (fake *FakeNginxUpdater) UpdateUpstreamServersCalls(stub func(*agent.Deployment, dataplane.Configuration) bool) {
 	fake.updateUpstreamServersMutex.Lock()
 	defer fake.updateUpstreamServersMutex.Unlock()
 	fake.UpdateUpstreamServersStub = stub
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServersArgsForCall(i int) (*agent.Deployment, dataplane.Configuration) {
+	fake.updateUpstreamServersMutex.RLock()
+	defer fake.updateUpstreamServersMutex.RUnlock()
+	argsForCall := fake.updateUpstreamServersArgsForCall[i]
+	return argsForCall.arg1, argsForCall.arg2
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServersReturns(result1 bool) {
+	fake.updateUpstreamServersMutex.Lock()
+	defer fake.updateUpstreamServersMutex.Unlock()
+	fake.UpdateUpstreamServersStub = nil
+	fake.updateUpstreamServersReturns = struct {
+		result1 bool
+	}{result1}
+}
+
+func (fake *FakeNginxUpdater) UpdateUpstreamServersReturnsOnCall(i int, result1 bool) {
+	fake.updateUpstreamServersMutex.Lock()
+	defer fake.updateUpstreamServersMutex.Unlock()
+	fake.UpdateUpstreamServersStub = nil
+	if fake.updateUpstreamServersReturnsOnCall == nil {
+		fake.updateUpstreamServersReturnsOnCall = make(map[int]struct {
+			result1 bool
+		})
+	}
+	fake.updateUpstreamServersReturnsOnCall[i] = struct {
+		result1 bool
+	}{result1}
 }
 
 func (fake *FakeNginxUpdater) Invocations() map[string][][]interface{} {

internal/mode/static/nginx/agent/grpc/messenger/messenger_test.go
@@ -1,0 +1,125 @@
+package messenger_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	v1 "github.com/nginx/agent/v3/api/grpc/mpi/v1"
+	. "github.com/onsi/gomega"
+	"google.golang.org/grpc"
+
+	"github.com/nginx/nginx-gateway-fabric/internal/mode/static/nginx/agent/grpc/messenger"
+)
+
+type mockServer struct {
+	grpc.ServerStream
+	sendChan chan *v1.ManagementPlaneRequest
+	recvChan chan *v1.DataPlaneResponse
+}
+
+func (m *mockServer) Send(msg *v1.ManagementPlaneRequest) error {
+	m.sendChan <- msg
+	return nil
+}
+
+func (m *mockServer) Recv() (*v1.DataPlaneResponse, error) {
+	msg, ok := <-m.recvChan
+	if !ok {
+		return nil, errors.New("channel closed")
+	}
+	return msg, nil
+}
+
+type mockErrorServer struct {
+	grpc.ServerStream
+	sendChan chan *v1.ManagementPlaneRequest
+	recvChan chan *v1.DataPlaneResponse
+}
+
+func (m *mockErrorServer) Send(_ *v1.ManagementPlaneRequest) error {
+	return errors.New("error sending to server")
+}
+
+func (m *mockErrorServer) Recv() (*v1.DataPlaneResponse, error) {
+	<-m.recvChan
+	return nil, errors.New("error received from server")
+}
+
+func createServer() *mockServer {
+	return &mockServer{
+		sendChan: make(chan *v1.ManagementPlaneRequest, 1),
+		recvChan: make(chan *v1.DataPlaneResponse, 1),
+	}
+}
+
+func createErrorServer() *mockErrorServer {
+	return &mockErrorServer{
+		sendChan: make(chan *v1.ManagementPlaneRequest, 1),
+		recvChan: make(chan *v1.DataPlaneResponse, 1),
+	}
+}
+
+func TestSend(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := createServer()
+	msgr := messenger.New(server)
+
+	go msgr.Run(ctx)
+
+	msg := &v1.ManagementPlaneRequest{
+		MessageMeta: &v1.MessageMeta{
+			MessageId: "test",
+		},
+	}
+	g.Expect(msgr.Send(ctx, msg)).To(Succeed())
+
+	g.Eventually(server.sendChan).Should(Receive(Equal(msg)))
+
+	cancel()
+
+	g.Expect(msgr.Send(ctx, &v1.ManagementPlaneRequest{})).ToNot(Succeed())
+}
+
+func TestMessages(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := createServer()
+	msgr := messenger.New(server)
+
+	go msgr.Run(ctx)
+
+	msg := &v1.DataPlaneResponse{InstanceId: "test"}
+	server.recvChan <- msg
+
+	g.Eventually(msgr.Messages()).Should(Receive(Equal(msg)))
+}
+
+func TestErrors(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := createErrorServer()
+	msgr := messenger.New(server)
+
+	go msgr.Run(ctx)
+
+	g.Expect(msgr.Send(ctx, &v1.ManagementPlaneRequest{})).To(Succeed())
+	g.Eventually(msgr.Errors()).Should(Receive(MatchError("error sending to server")))
+
+	server.recvChan <- &v1.DataPlaneResponse{}
+
+	g.Eventually(msgr.Errors()).Should(Receive(MatchError("error received from server")))
+}

internal/mode/static/nginx/config/configfakes/fake_generator.go
@@ -4,41 +4,41 @@ package configfakes
 import (
 	"sync"
 
-	"github.com/nginx/nginx-gateway-fabric/internal/framework/file"
+	"github.com/nginx/nginx-gateway-fabric/internal/mode/static/nginx/agent"
 	"github.com/nginx/nginx-gateway-fabric/internal/mode/static/nginx/config"
 	"github.com/nginx/nginx-gateway-fabric/internal/mode/static/state/dataplane"
 )
 
 type FakeGenerator struct {
-	GenerateStub        func(dataplane.Configuration) []file.File
+	GenerateStub        func(dataplane.Configuration) []agent.File
 	generateMutex       sync.RWMutex
 	generateArgsForCall []struct {
 		arg1 dataplane.Configuration
 	}
 	generateReturns struct {
-		result1 []file.File
+		result1 []agent.File
 	}
 	generateReturnsOnCall map[int]struct {
-		result1 []file.File
+		result1 []agent.File
 	}
-	GenerateDeploymentContextStub        func(dataplane.DeploymentContext) (file.File, error)
+	GenerateDeploymentContextStub        func(dataplane.DeploymentContext) (agent.File, error)
 	generateDeploymentContextMutex       sync.RWMutex
 	generateDeploymentContextArgsForCall []struct {
 		arg1 dataplane.DeploymentContext
 	}
 	generateDeploymentContextReturns struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}
 	generateDeploymentContextReturnsOnCall map[int]struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeGenerator) Generate(arg1 dataplane.Configuration) []file.File {
+func (fake *FakeGenerator) Generate(arg1 dataplane.Configuration) []agent.File {
 	fake.generateMutex.Lock()
 	ret, specificReturn := fake.generateReturnsOnCall[len(fake.generateArgsForCall)]
 	fake.generateArgsForCall = append(fake.generateArgsForCall, struct {
@@ -63,7 +63,7 @@ func (fake *FakeGenerator) GenerateCallCount() int {
 	return len(fake.generateArgsForCall)
 }
 
-func (fake *FakeGenerator) GenerateCalls(stub func(dataplane.Configuration) []file.File) {
+func (fake *FakeGenerator) GenerateCalls(stub func(dataplane.Configuration) []agent.File) {
 	fake.generateMutex.Lock()
 	defer fake.generateMutex.Unlock()
 	fake.GenerateStub = stub
@@ -76,30 +76,30 @@ func (fake *FakeGenerator) GenerateArgsForCall(i int) dataplane.Configuration {
 	return argsForCall.arg1
 }
 
-func (fake *FakeGenerator) GenerateReturns(result1 []file.File) {
+func (fake *FakeGenerator) GenerateReturns(result1 []agent.File) {
 	fake.generateMutex.Lock()
 	defer fake.generateMutex.Unlock()
 	fake.GenerateStub = nil
 	fake.generateReturns = struct {
-		result1 []file.File
+		result1 []agent.File
 	}{result1}
 }
 
-func (fake *FakeGenerator) GenerateReturnsOnCall(i int, result1 []file.File) {
+func (fake *FakeGenerator) GenerateReturnsOnCall(i int, result1 []agent.File) {
 	fake.generateMutex.Lock()
 	defer fake.generateMutex.Unlock()
 	fake.GenerateStub = nil
 	if fake.generateReturnsOnCall == nil {
 		fake.generateReturnsOnCall = make(map[int]struct {
-			result1 []file.File
+			result1 []agent.File
 		})
 	}
 	fake.generateReturnsOnCall[i] = struct {
-		result1 []file.File
+		result1 []agent.File
 	}{result1}
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContext(arg1 dataplane.DeploymentContext) (file.File, error) {
+func (fake *FakeGenerator) GenerateDeploymentContext(arg1 dataplane.DeploymentContext) (agent.File, error) {
 	fake.generateDeploymentContextMutex.Lock()
 	ret, specificReturn := fake.generateDeploymentContextReturnsOnCall[len(fake.generateDeploymentContextArgsForCall)]
 	fake.generateDeploymentContextArgsForCall = append(fake.generateDeploymentContextArgsForCall, struct {
@@ -124,7 +124,7 @@ func (fake *FakeGenerator) GenerateDeploymentContextCallCount() int {
 	return len(fake.generateDeploymentContextArgsForCall)
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContextCalls(stub func(dataplane.DeploymentContext) (file.File, error)) {
+func (fake *FakeGenerator) GenerateDeploymentContextCalls(stub func(dataplane.DeploymentContext) (agent.File, error)) {
 	fake.generateDeploymentContextMutex.Lock()
 	defer fake.generateDeploymentContextMutex.Unlock()
 	fake.GenerateDeploymentContextStub = stub
@@ -137,28 +137,28 @@ func (fake *FakeGenerator) GenerateDeploymentContextArgsForCall(i int) dataplane
 	return argsForCall.arg1
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContextReturns(result1 file.File, result2 error) {
+func (fake *FakeGenerator) GenerateDeploymentContextReturns(result1 agent.File, result2 error) {
 	fake.generateDeploymentContextMutex.Lock()
 	defer fake.generateDeploymentContextMutex.Unlock()
 	fake.GenerateDeploymentContextStub = nil
 	fake.generateDeploymentContextReturns = struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}{result1, result2}
 }
 
-func (fake *FakeGenerator) GenerateDeploymentContextReturnsOnCall(i int, result1 file.File, result2 error) {
+func (fake *FakeGenerator) GenerateDeploymentContextReturnsOnCall(i int, result1 agent.File, result2 error) {
 	fake.generateDeploymentContextMutex.Lock()
 	defer fake.generateDeploymentContextMutex.Unlock()
 	fake.GenerateDeploymentContextStub = nil
 	if fake.generateDeploymentContextReturnsOnCall == nil {
 		fake.generateDeploymentContextReturnsOnCall = make(map[int]struct {
-			result1 file.File
+			result1 agent.File
 			result2 error
 		})
 	}
 	fake.generateDeploymentContextReturnsOnCall[i] = struct {
-		result1 file.File
+		result1 agent.File
 		result2 error
 	}{result1, result2}
 }